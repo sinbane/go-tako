6b92ae6e2a5f3efa5fac8ac22cde615176f765ac
Mark Hong <zhengjun.hong@ninjavan.co>
2024-09-26 00:41:48 +0800

    update unique request id

--- middleware/request_id.go
+++ middleware/request_id.go
@@ -4,27 +4,37 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
+	"fmt"
+	"log"
 	"net/http"
 	"strings"
+	"sync/atomic"
 
 	"github.com/sinbane/tako/config"
 )
+
+// count is used to generate a unique request ID for each incoming request
+var count uint64
 
 // RequestID is a middleware that generates a unique request ID for each incoming request
 func RequestID(cfg *config.Config) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			requestID := encode(r.RemoteAddr, 16)
-			w.Header().Set(HeaderRequestId, requestID)
+			prefix := encode(cfg.ServerId, 28)
+			id := atomic.AddUint64(&count, 1)
+			r.Header.Set(HeaderRequestId, fmt.Sprintf("%s-%04d", prefix, id))
 			next.ServeHTTP(w, r)
 		})
 	}
 }
 
-// Generate a encoded string of a fixed length
+// Generate a random encoded string of a fixed length
 func encode(input string, fixedLength int) string {
 	var buf [12]byte
-	rand.Read(buf[:])
+	_, err := rand.Read(buf[:])
+	if err != nil {
+		log.Printf("Error generating random bytes: %v\n", err)
+	}
 	hasher := sha256.New()
 	hasher.Write(append([]byte(input), buf[:]...))
 	hashBytes := hasher.Sum(nil)
